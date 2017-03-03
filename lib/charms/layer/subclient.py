dee1f9919a78493c306caf0798bf28385c8ee7ce
sborny <sander.borny@ugent.be>
2017-03-03 11:10:21 +0100

    Added requests exceptions

--- lib/charms/layer/subclient.py
+++ lib/charms/layer/subclient.py
@@ -4,12 +4,16 @@ from charmhelpers.core import hookenv
 
 config = hookenv.config()
 
-
 def subscribe(subscriber, config):
-	headers = {'Content-type': 'application/json'}
-	r = requests.put("http://" + subscriber + "/subscribe", data=json.dumps(config), headers=headers)
+    try:
+        headers = {'Content-type': 'application/json'}
+        r = requests.put("http://" + subscriber + "/subscribe", data=json.dumps(config), headers=headers)
+    except requests.exceptions.RequestException as e:
+        hookenv.log(e)
 
 def unsubscribe(subscriber, config):
-	headers = {'Content-type': 'application/json'}
-	hookenv.log(json.dumps(config))
-	r = requests.delete("http://" + subscriber + "/unsubscribe", data=json.dumps(config), headers=headers)
+    try:
+        headers = {'Content-type': 'application/json'}
+        r = requests.delete("http://" + subscriber + "/unsubscribe", data=json.dumps(config), headers=headers)
+    except requests.exceptions.RequestException as e:
+        hookenv.log(e)
