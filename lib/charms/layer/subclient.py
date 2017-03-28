b80db0d0123d9cb8fa89a1c36ac5c06096a1459c
sborny <sander.borny@ugent.be>
2017-03-28 16:49:44 +0200

    Support for basic auth

--- lib/charms/layer/subclient.py
+++ lib/charms/layer/subclient.py
@@ -1,17 +1,38 @@
 import requests
 import json
 from charmhelpers.core import hookenv
+from requests.auth import HTTPBasicAuth
+
+config = hookenv.config()
 
 def subscribe(subscriber, payload):
     try:
         headers = {'Content-type': 'application/json'}
-        r = requests.put("http://" + subscriber + "/subscribe", data=json.dumps(payload), headers=headers)
+        if config['credentials']:
+            credentials = config['credentials'].split(' ')
+            r = requests.put("https://" + subscriber + "/subscribe",
+                             data=json.dumps(payload),
+                             headers=headers,
+                             auth=HTTPBasicAuth(credentials[0], credentials[1]))
+        else:
+            r = requests.put("http://" + subscriber + "/subscribe",
+                             data=json.dumps(payload),
+                             headers=headers)
     except requests.exceptions.RequestException as e:
         hookenv.log(e)
 
 def unsubscribe(subscriber, payload):
     try:
         headers = {'Content-type': 'application/json'}
-        r = requests.delete("http://" + subscriber + "/unsubscribe", data=json.dumps(payload), headers=headers)
+        if config['credentials']:
+            credentials = config['credentials'].split(' ')
+            r = requests.delete("http://" + subscriber + "/unsubscribe", 
+                                data=json.dumps(payload), 
+                                headers=headers,
+                                auth=HTTPBasicAuth(credentials[0], credentials[1]))
+        else:
+            r = requests.delete("http://" + subscriber + "/unsubscribe",
+                                data=json.dumps(payload), 
+                                headers=headers)
     except requests.exceptions.RequestException as e:
         hookenv.log(e)
